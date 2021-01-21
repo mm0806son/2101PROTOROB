0f7153a7496ad6f5d2e6de37984fcfa7b8d6a597
mm0806son <73909893+mm0806son@users.noreply.github.com>
2021-01-21 10:31:06 +0100

    v2.0 Interruption
    使用中断来读编码器数据，防止干扰测温。

--- together_v1.0/together_v1.0.ino
+++ together_v1.0/together_v1.0.ino
@@ -2,8 +2,8 @@
 #include <Wire.h>
 #include <Adafruit_MPL3115A2.h>
 
-Encoder myEnc(2, 3);
-float Position_KP = 5, Position_KI = 0, Position_KD = 1000;
+Encoder myEnc(19, 18);
+float Position_KP = 0.1, Position_KI = 0, Position_KD = 10000;
 int motor = 6;
 int order;
 int target;
@@ -15,19 +15,35 @@ void setup() {
   pinMode (motor, OUTPUT);
   pinMode (direct, OUTPUT);
   Serial.begin(9600);
-  Serial.println("Basic Encoder Test:");
-
+  Serial.println("Start Test:");
+  cli(); // Désactive l'interruption globale
+  bitClear (TCCR2A, WGM20); // WGM20 = 0
+  bitClear (TCCR2A, WGM21); // WGM21 = 0
+  TCCR2B = 0b00000101; // Clock / 128 soit 16 micro-s et WGM22 = 0
+  TIMSK2 = 0b00000001; // Interruption locale autorisée par TOIE2
+  sei(); // Active l'interruption globale
 }
-
+byte varCompteur = 0; // La variable compteur
 long nowPosition  = -999;
 
-void loop() {
-  long newPosition = myEnc.read();
-  if (newPosition - nowPosition>50||newPosition - nowPosition<-50) {
-    nowPosition = newPosition;
-    Serial.println(nowPosition);
+// Routine d'interruption
+ISR(TIMER2_OVF_vect) {
+  TCNT2 = 128 - 125; // 125 x 8 µS = 1 ms
+  if (varCompteur++ > 10) { // 50 * 1 ms = 50 ms (demi-période)
+    varCompteur = 0;
+    long newPosition = myEnc.read();
+    if (newPosition - nowPosition > 20 || newPosition - nowPosition < -20) {
+      nowPosition = newPosition;
+      Serial.print("nowPosition: "); Serial.println(nowPosition);
+      //target = map(temperature, 10, 30, 0, 475);
+      //Serial.print("target: "); Serial.println(target);
+      //turn(nowPosition, target);turn放在这里就不能正常运行，好神奇
+    }
+    turn(nowPosition, target);
   }
+}
 
+void loop() {
   if (! baro.begin()) {
     Serial.println("Couldnt find sensor");
     delay(1000);
@@ -35,10 +51,12 @@ void loop() {
   }
 
   temperature = baro.getTemperature();
+  Serial.print("temperature: "); Serial.println(temperature);
   target = map(temperature, 10, 30, 0, 475);
-  Serial.print(temperature); Serial.println(target);
-  turn(nowPosition, target);
-  delay(10);
+  Serial.print("target: "); Serial.println(target);
+
+
+  delay(1000);
 }
 
 int PWM_PID (int nowPosition, int target)
@@ -66,10 +84,10 @@ int PWM_PID (int nowPosition, int target)
 }
 
 void turn(int nowPosition, int target) {
-//  if (target > nowPosition) {
-//    digitalWrite(direct, HIGH);
-//  } else {
-//    digitalWrite(direct, LOW);
-//  }
+  //  if (target > nowPosition) {
+  //    digitalWrite(direct, HIGH);
+  //  } else {
+  //    digitalWrite(direct, LOW);
+  //  }
   analogWrite(motor, PWM_PID (nowPosition, target));
 }
