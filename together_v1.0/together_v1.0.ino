0e0395ee38e90da98583444ed98c609b1d11597f
mm0806son <73909893+mm0806son@users.noreply.github.com>
2021-01-21 23:27:33 +0100

    together v3.0
    加入了校准

--- together_v1.0/together_v1.0.ino
+++ together_v1.0/together_v1.0.ino
@@ -11,15 +11,59 @@ int direct = 7;
 Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
 float temperature;
 char comchar;
+long calib;
+long nowPosition;
 
 void setup() {
+
+  Serial.begin(9600);
+  Serial.println("Start Test:");
   pinMode (motor, OUTPUT);
   pinMode (direct, OUTPUT);
-  Serial.begin(9600);
-  while (Serial.read() >= 0) {} //clear serialbuffer
-  Serial.println("Start Test:");
+  while (Serial.read() >= 0) {} ;//clear serialbuffer
+
+  Serial.println("Calibrate:");
+  do {
+    //Serial.println("Calibrate:");
+    while (Serial.available() > 0) {
+      comchar = Serial.read();//读串口第一个字节
+      Serial.print("Serial.read: ");
+      Serial.println(comchar);
+      if (comchar == '-') {
+        digitalWrite(direct, LOW);
+        analogWrite(motor, 50);
+        delay(40);
+        analogWrite(motor, 0);
+        while (Serial.read() >= 0) {}
+      } else if (comchar == '+') {
+        digitalWrite(direct, HIGH);
+        analogWrite(motor, 50);
+        delay(40);
+        analogWrite(motor, 0);
+        while (Serial.read() >= 0) {}
+      } else if (comchar == '1') {
+        digitalWrite(direct, HIGH);
+        analogWrite(motor, 50);
+        delay(100);
+        analogWrite(motor, 0);
+        while (Serial.read() >= 0) {}
+      } else if (comchar == '2') {
+        digitalWrite(direct, LOW);
+        analogWrite(motor, 50);
+        delay(100);
+        analogWrite(motor, 0);
+        while (Serial.read() >= 0) {}
+      } else if (comchar != '='){
+        Serial.println("Error!");
+        while (Serial.read() >= 0) {}
+      }
+      delay(100);
+    }
+  } while (comchar != '=');
+  while (Serial.read() >= 0) {};
+  calib = myEnc.read();
+  Serial.print("Calibration finished:");Serial.println(calib);
   cli(); // Désactive l'interruption globale
-  //calibrate();
   bitClear (TCCR2A, WGM20); // WGM20 = 0
   bitClear (TCCR2A, WGM21); // WGM21 = 0
   TCCR2B = 0b00000101; // Clock / 128 soit 16 micro-s et WGM22 = 0
@@ -27,7 +71,7 @@ void setup() {
   sei(); // Active l'interruption globale
 }
 byte varCompteur = 0; // La variable compteur
-long nowPosition = -999;
+
 
 // Routine d'interruption
 ISR(TIMER2_OVF_vect) {
@@ -37,10 +81,10 @@ ISR(TIMER2_OVF_vect) {
     long newPosition = myEnc.read();
     if (newPosition - nowPosition > 10 || newPosition - nowPosition < -10) {
       nowPosition = newPosition;
-      Serial.print("nowPosition: "); Serial.println(nowPosition);
+      Serial.print("nowPosition: "); Serial.println(nowPosition-calib);
     }
-    //Serial.print("nowPosition: "); Serial.println(nowPosition);
-    turn(nowPosition, target);
+    //Serial.print("nowPosition: "); Serial.println(nowPosition-calib);
+    turn(nowPosition-calib, target);
   }
 }
 
@@ -77,47 +121,4 @@ void turn (int nowPosition, int target)
     Pwm = 0;
   }
   analogWrite(motor, Pwm);
-}
-
-void calibrate() {
-  // read data from serial port to calibrate
-  while (1) {
-    while (Serial.available() > 0) {
-      comchar = Serial.read();//读串口第一个字节
-      Serial.print("Serial.read: ");
-      Serial.println(comchar);
-      if (comchar == '-') {
-        digitalWrite(direct, LOW);
-        analogWrite(motor, 50);
-        delay(20);
-        analogWrite(motor, 0);
-        while (Serial.read() >= 0) {}
-      } else if (comchar == '+') {
-        digitalWrite(direct, HIGH);
-        analogWrite(motor, 50);
-        delay(20);
-        analogWrite(motor, 0);
-        while (Serial.read() >= 0) {}
-      } else if (comchar == '1') {
-        digitalWrite(direct, HIGH);
-        analogWrite(motor, 50);
-        delay(100);
-        analogWrite(motor, 0);
-        while (Serial.read() >= 0) {}
-      } else if (comchar == '2') {
-        digitalWrite(direct, LOW);
-        analogWrite(motor, 50);
-        delay(100);
-        analogWrite(motor, 0);
-        while (Serial.read() >= 0) {}
-      } else if (comchar == '=') {
-        while (Serial.read() >= 0) {}
-        return;
-      } else {
-        Serial.println("Error!");
-        while (Serial.read() >= 0) {}
-      }
-      delay(100);
-    }
-  }
 }
