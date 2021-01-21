e237215aba590243b28142383ae275879ac0e2b4
mm0806son <73909893+mm0806son@users.noreply.github.com>
2021-01-21 13:05:57 +0100

    v2.0_draw figure
    利用串口绘图器显示电机运动曲线，写报告用。

--- Encoder_PID/Encoder_PID.ino
+++ Encoder_PID/Encoder_PID.ino
@@ -2,7 +2,7 @@
 
 
 Encoder myEnc(19, 18);
-float Position_KP = 0.5, Position_KI = 0, Position_KD = 1000;
+float Position_KP = 0.5, Position_KI = 0, Position_KD = 10000;
 int motor = 6;
 int order;
 int target;
@@ -12,7 +12,7 @@ void setup() {
   pinMode (motor, OUTPUT);
   pinMode (direct, OUTPUT);
   Serial.begin(9600);
-  Serial.println("Basic Encoder Test:");
+  //Serial.println("Basic Encoder Test:");
 
 }
 
@@ -20,17 +20,22 @@ long nowPosition  = -999;
 
 void loop() {
   long newPosition = myEnc.read();
-  if (newPosition - nowPosition>50||newPosition - nowPosition<-50) {
+  if (newPosition - nowPosition > 50 || newPosition - nowPosition < -50) {
     nowPosition = newPosition;
+    Serial.print(target);
+    Serial.print(",");
     Serial.println(nowPosition);
   }
 
-  target = 5000;
+  target = 1000;
   turn(nowPosition, target);
+  //  Serial.print(target);
+  //  Serial.print(",");
+  //  Serial.println(nowPosition);
   delay(10);
 }
 
-int PWM_PID (int nowPosition, int target)
+int turn (int nowPosition, int target)
 {
   static float Bias, Pwm, Integral_bias, Last_Bias;
   Bias = target - nowPosition;
@@ -43,15 +48,9 @@ int PWM_PID (int nowPosition, int target)
   Integral_bias += Bias;
   Pwm = Position_KP * Bias + Position_KI * Integral_bias + Position_KD * (Bias - Last_Bias);
   Last_Bias = Bias;
-  if (Pwm > 255) {
-    return 255;
+  if (Pwm > 50) {
+    Pwm = 50;
   }
-  else {
-    return Pwm;
-  }
+  analogWrite(motor, Pwm);
 }
 
-void turn(int nowPosition, int target) {
-
-  analogWrite(motor, PWM_PID (nowPosition, target));
-}
